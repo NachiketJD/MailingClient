287ecc93251d690d2b9f0911dab7d331f539b91c
Nachiket Jayant Deshpande <nachiketdeshpande2004@gmail.com>
2025-06-16 11:40:12 +0530

    ehlo

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
 import smtplib
 
 server = smtplib.SMTP('smtp.gmail.com', 25) # This line of code, is used to connect to the Gmail server 
+server.ehlo() # This line of code, is used to send an email to the Gmail server
