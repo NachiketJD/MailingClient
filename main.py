f7cdb144d0d3e83199ac20feb3d6d886a91829c2
Nachiket Jayant Deshpande <nachiketdeshpande2004@gmail.com>
2025-06-16 11:42:49 +0530

    Decrypt the passwd

--- main.py
+++ main.py
@@ -2,3 +2,6 @@ import smtplib
 
 server = smtplib.SMTP('smtp.gmail.com', 25) # This line of code, is used to connect to the Gmail server 
 server.ehlo() # This line of code, is used to send an email to the Gmail server
+
+with open('password.txt', 'r') as f:
+    password = f.read() # This line of code, is used to read the password from the password.txt file the password is encrypted and stored in the password.txt file then, it is read and used to login to the Gmail server.
