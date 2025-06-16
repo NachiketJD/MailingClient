4effc6de808b53c58a10b4969cce9eae3051352d
Nachiket Jayant Deshpande <nachiketdeshpande2004@gmail.com>
2025-06-16 11:47:57 +0530

    Comment1

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import smtplib
-from email import encoders
+from email import encoders # This is used to encode the attachment
 from email.mime.multipart import MIMEMultipart
 from email.mime.base import MIMEBase
 from email.mime.text import MIMEText
